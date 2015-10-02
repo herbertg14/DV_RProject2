8e70d527ec2d7a65074e36e25a50069cf77c2e36
herbertg14 <herbert.gutierrez@utexas.edu>
2015-10-02 00:08:23 -0500

    Making final changes to the rmd file

--- 02 Data Wrangling/Project2_ETL.R
+++ 02 Data Wrangling/Project2_ETL.R
@@ -1,20 +1,18 @@
 require("ggplot2")
-require("ggthemes")
 require("gplots")
 require("grid")
 require("RCurl")
 require("reshape2")
-require("rstudio")
 require("tableplot")
 require("tidyr")
 require("dplyr")
 require("jsonlite")
-require("extrafont")
 require("lubridate")
 
 
 # Import Data from SQL database
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from COD"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_riw223', PASS='orcl_riw223', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-#summary(df)
-#head(df)
+
+summary(df)
+head(df)
 
